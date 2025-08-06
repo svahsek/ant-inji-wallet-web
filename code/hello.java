9fac03954a96368451735bac791bbaeb1aff136c
svahsek <ks@keshavs.com>
2025-08-06 21:27:09 +0530

    6 aug 1

---
+++ code/hello.java
@@ -0,0 +1,9 @@
+package code;
+
+// tag::my-component-web[]
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+// end::my-component-web[]
