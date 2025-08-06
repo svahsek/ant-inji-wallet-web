9e047e6cadfa15bd12c205d8f9de510769b1deab
svahsek <ks@keshavs.com>
2025-08-06 22:00:12 +0530

    bawal

--- code/hello.java
+++ code/hello.java
@@ -1,9 +1,9 @@
 package code;
 
-// tag::my-component-web[]
+// tag::my-component[]
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
-// end::my-component-web[]
+// end::my-component[]
