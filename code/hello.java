53fd122cc251e85f1bbd69fe222f9c07d35b9c27
svahsek <ks@keshavs.com>
2025-08-07 09:10:18 +0530

    2nd

--- code/hello.java
+++ code/hello.java
@@ -1,9 +1,9 @@
 package code;
 
-// tag::my-component[]
+// tag::java-code[]
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
-// end::my-component[]
+// end::java-code[]
